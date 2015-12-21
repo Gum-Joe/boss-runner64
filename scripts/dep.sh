a3d27d9f2b8df32bc924ed5422b4d1c33884d94f
Gum-Joe <kishansambhi@hotmail.co.uk>
2015-12-21 17:16:35 +0000

    Change rvm install to use binary

--- scripts/dep.sh
+++ scripts/dep.sh
@@ -45,7 +45,7 @@ nvm install stable
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
 source /etc/profile.d/rvm.sh
-rvm install 2.2.4
+sudo rvm install ruby-head --binary
 # Install bundle
 gem install bundle
 # Done!
